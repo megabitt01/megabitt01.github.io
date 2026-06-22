bcb275470569bb486a700c9c03c17fdfa078e431
Jack Bittner <jack.bittner@outlook.com>
2026-06-21 19:21:31 -0500

    feat: preliminary changes for new site look

---
+++ src/components/NewHeader.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import SkeuoImage from "./SkeuoImage";
+
+export default function Header() {
+    const [ cursor, setCursor ] = useState(true);
+    return(
+        <>
+        <div className="new-header-bar">
+            <div className="new-header-content">
+                <img src="https://avatars.githubusercontent.com/u/71474581?v=4" className="new-header-image" />
+                <div className="new-header-text">
+                    <h2>
+                        thejackbitt{cursor ? "█" : ""}
+                    </h2>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
